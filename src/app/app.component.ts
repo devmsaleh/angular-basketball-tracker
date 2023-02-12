dca81da5539185783aac62b3e8e30d9ddb8744c1
DevTeam <dev@mc.gov.sa>
2023-02-12 14:03:33 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basketball-tracker';
+  title = 'basketball-tracker'; 
 }
